ba92d63c8566777d2c4b6e88a13d92c2965b283e
jcredding <TempestTTU@gmail.com>
2011-09-23 17:40:50 -0500

    first commit

---
+++ lib/assert-mocha.rb
@@ -0,0 +1,7 @@
+require "assert-mocha/version"
+
+module Assert
+  module Mocha
+    # Your code goes here...
+  end
+end
---
+++ lib/assert-mocha/version.rb
@@ -0,0 +1,5 @@
+module Assert
+  module Mocha
+    VERSION = "0.0.1"
+  end
+end
